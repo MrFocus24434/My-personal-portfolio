d96715f5cfe5976ab9d85cac2382115ca39ba106
MrFocus@24434 <Mr.focusofficial24434@gmail.com>
2024-04-19 09:22:23 +0100

    my first commit

---
+++ Skills/main.js
@@ -0,0 +1,27 @@
+// const itmes = document.querySelectorAll
+// ('#timeline li');
+
+// const isInViewport = el => {
+//   const rect = el.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <=
+//       (window.innerHeight ||
+//       document.documentElement.clientHeight) &&
+//       rect.right <= (window.innerWidth ||
+//       document.documentElement.clientWidth)
+//   );
+// };
+
+// const run = () =>
+// itmes.forEach(item => {
+//   if (isInViewport(item)) {
+//     item.classlist.add('show');
+//   }
+// });
+
+// //event
+// window.addEventListener('load', run);
+// window.addEventListener('resize', run);
+// window.addEventListener('scroll', run);
